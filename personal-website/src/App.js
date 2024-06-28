179c63b0a5e8337554837e526680124c0bb2b14d
jel084 <130106981+jel084@users.noreply.github.com>
2024-06-28 02:01:58 -0700

    set up

---
+++ personal-website/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <strong> Hello world, I am <span className="text-blue">Jeanine Le</span></strong> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
